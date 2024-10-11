55bca2752d8e9b34fe43b119b00c9cac0f7b7c86
Abdelrahman Nasser <abdelrahmanhydara@gmail.com>
2024-10-11 18:55:12 +0300

    - Just teasing

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main()
+{
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget
+{
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)
+  {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter',
       theme: ThemeData(
         // This is the theme of your application.
         //
