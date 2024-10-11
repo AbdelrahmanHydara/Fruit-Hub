5e0654f916e9285c380b8a183a487a35fec50ec3
Abdelrahman Nasser <abdelrahmanhydara@gmail.com>
2024-10-11 21:20:15 +0300

    - splash ui logic done

--- lib/features/splash/screens/splash_screen.dart
+++ lib/features/splash/screens/splash_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'widgets/splash_screen_body.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SplashScreenBody(),
+    );
   }
 }
