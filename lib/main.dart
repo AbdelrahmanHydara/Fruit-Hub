0343f922939812facbc8f0497d9d36068b05aeaa
Abdelrahman Nasser <abdelrahmanhydara@gmail.com>
2024-12-06 16:14:30 +0200

    - added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_app/core/services/shared_pref_singleton.dart';
+import 'package:fruits_app/firebase_options.dart';
 import 'package:fruits_app/fruits.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await SharedPrefSingleton.init();
   runApp(const FruitsApp());
 }
